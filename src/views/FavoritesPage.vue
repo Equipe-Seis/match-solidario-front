02632eccf6061640283bc777d355ed76db31f35a
Caroline Caprini <caroline.caprini@hotmail.com>
2025-10-19 18:08:51 -0300

    added remove button on favorites page

--- src/views/FavoritesPage.vue
+++ src/views/FavoritesPage.vue
@@ -13,18 +13,13 @@
         <ion-list lines="full">
           <ion-item>
             <ion-label>Pokémon Yellow</ion-label>
-          </ion-item>
-          <ion-item>
-            <ion-label>Mega Man X</ion-label>
-          </ion-item>
-          <ion-item>
-            <ion-label>The Legend of Zelda</ion-label>
-          </ion-item>
-          <ion-item>
-            <ion-label>Pac-Man</ion-label>
-          </ion-item>
-          <ion-item>
-            <ion-label>Super Mario World</ion-label>
+            <ion-button shape="round" fill="clear" size="large" @click="">
+              <ion-icon
+                slot="icon-only"
+                color="danger"
+                :icon="closeCircle"
+              ></ion-icon>
+            </ion-button>
           </ion-item>
         </ion-list>
       </div>
@@ -34,6 +29,12 @@
 
 <script setup lang="ts">
 import { IonContent, IonItem, IonLabel, IonList } from "@ionic/vue";
+import {
+  closeCircle,
+  closeCircleOutline,
+  closeOutline,
+  timeOutline,
+} from "ionicons/icons";
 </script>
 
 <style scoped lang="scss" src="./css/RegisterPage.scss"></style>
